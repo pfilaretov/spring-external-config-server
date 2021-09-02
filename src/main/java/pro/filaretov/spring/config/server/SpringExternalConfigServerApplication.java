721d4160ce02798b2de44e60063aafa3a90cfb60
Petr Filaretov <petr_filaretov@epam.com>
2021-09-02 16:31:55 +0300

    initial commit

---
+++ src/main/java/pro/filaretov/spring/config/server/SpringExternalConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExternalConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExternalConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/filaretov/spring/config/server/SpringExternalConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExternalConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
