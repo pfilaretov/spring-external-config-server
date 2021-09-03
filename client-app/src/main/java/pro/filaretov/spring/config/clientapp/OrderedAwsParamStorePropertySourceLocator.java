3333e0c9a38ff1429d728a1ab74cbac21646f61c
Petr Filaretov <petr.filaretov@gmail.com>
2021-09-03 13:35:52 +0300

    AWS Parameter Store has a higher precedence than Config Server

---
+++ client-app/src/main/java/pro/filaretov/spring/config/clientapp/OrderedAwsParamStorePropertySourceLocator.java
@@ -0,0 +1,34 @@
+package pro.filaretov.spring.config.clientapp;
+
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import org.springframework.cloud.aws.paramstore.AwsParamStoreProperties;
+import org.springframework.cloud.aws.paramstore.AwsParamStorePropertySourceLocator;
+import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+
+/**
+ * A dirty way to make AWS Parameter Store PropertySource has higher precedence than Config Server.
+ * <p/>
+ * <p><b>Additional context</b></p>
+ * Config Server defines {@link ConfigServicePropertySourceLocator} class with {@code @Order(0)}.
+ * On the other hand, AWS Parameter Store config defines {@link AwsParamStorePropertySourceLocator} which does not
+ * specify any {@code @Order}. This means it will have {@code Ordered.LOWEST_PRECEDENCE} by default.
+ * <p/>
+ * This class extends {@link AwsParamStorePropertySourceLocator} and does nothing but specifies {@code @Order}
+ * higher than Config Server does.
+ * <p/>
+ * NOTE: {@link AwsParamStorePropertySourceLocator} will still be loaded and uselessly retrieve properties from
+ * AWS Parameter Store.
+ */
+@Configuration
+@Primary
+@Order(-100)
+public class OrderedAwsParamStorePropertySourceLocator extends AwsParamStorePropertySourceLocator {
+
+    public OrderedAwsParamStorePropertySourceLocator(AWSSimpleSystemsManagement ssmClient,
+        AwsParamStoreProperties properties) {
+        super(ssmClient, properties);
+    }
+}
