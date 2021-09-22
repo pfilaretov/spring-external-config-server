a6c2de9f036632b2fe8b1e325e89fdf6c2e202fe
Petr Filaretov <petr.filaretov@gmail.com>
2021-09-22 15:06:14 +0300

    move AWS ParamStore bootstrap configuration to the spring boot starter module

--- aws-property-source-spring-boot-starter/src/main/java/pro/filaretov/spring/config/aws/OrderedAwsParamStorePropertySourceLocator.java
+++ aws-property-source-spring-boot-starter/src/main/java/pro/filaretov/spring/config/aws/OrderedAwsParamStorePropertySourceLocator.java
@@ -3,14 +3,13 @@ package pro.filaretov.spring.config.aws;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
 import org.springframework.cloud.aws.paramstore.AwsParamStoreProperties;
 import org.springframework.cloud.aws.paramstore.AwsParamStorePropertySourceLocator;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.core.annotation.Order;
 
 /**
- * A dirty way to make AWS Parameter Store PropertySource have higher precedence than Config Server.
+ * This class is a way to make AWS Parameter Store PropertySource have higher precedence than Config Server.
  * <p/>
  * <p><b>Additional context</b></p>
- * Config Server defines {@link ConfigServicePropertySourceLocator} class with {@code @Order(0)}. On the other hand, AWS
+ * Config Server defines {@code ConfigServicePropertySourceLocator} class with {@code @Order(0)}. On the other hand, AWS
  * Parameter Store config defines {@link AwsParamStorePropertySourceLocator} which does not specify any {@code @Order}.
  * This means it will have {@code Ordered.LOWEST_PRECEDENCE} by default.
  * <p/>
